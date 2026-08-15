5a000ccc12fdd3b39fddcddb6909046d31a0a31c
kpphysicsacademy-design <kpphysicsacademy@gmail.com>
2026-08-15 11:23:08 +0530

    Connect website to KP Physics Academy Firebase project

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,11 @@
 // KP Physics Academy - Firebase Web App Configuration
-// Replace the values below with the config from Firebase Console > Project settings > Your apps > Web app.
+// Firebase Web App: KP Physics Academy Website
 export const firebaseConfig = {
-  apiKey: "PASTE_YOUR_FIREBASE_API_KEY",
+  apiKey: "AIzaSyByht3fBhAz9tC4i0N3laoI5NG5ve-FQlk",
   authDomain: "kp-physics-academy.firebaseapp.com",
   projectId: "kp-physics-academy",
   storageBucket: "kp-physics-academy.firebasestorage.app",
-  messagingSenderId: "PASTE_YOUR_MESSAGING_SENDER_ID",
-  appId: "PASTE_YOUR_FIREBASE_APP_ID"
+  messagingSenderId: "445260149248",
+  appId: "1:445260149248:web:c21427845e417de4ba1ee5",
+  measurementId: "G-BBQYTZY1FV"
 };
