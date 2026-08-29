611b2c471a914f25049e74df28d92d0611942f1d
kpphysicsacademy-design <kpphysicsacademy@gmail.com>
2026-08-29 08:05:35 +0530

    Route central Compete navigation to unified compete page

--- routes.js
+++ routes.js
@@ -3,24 +3,15 @@ window.SkillUpRoutes = {
   home: "index.html",
   learn: "learn.html",
   practice: "practice.html",
-  compete: "league.html",
-  challenge: "challenge.html",
-  league: "league.html",
+  compete: "compete.html",
+  challenge: "compete.html#challenges",
+  league: "compete.html#leagues",
   profile: "profile.html",
   login: "login.html"
 };
 
-/* Competition routes: League + Challenge */
-window.SkillUpRoutes.challengeDaily = "challenge.html";
-window.SkillUpRoutes.challengeConcepts = "challenge-concepts.html";
-window.SkillUpRoutes.challengeTimed = "challenge.html?mode=timed";
-
-/* Helper: navigate using the central route map */
 window.goToSkillUp = function(routeName) {
   const route = window.SkillUpRoutes[routeName];
-  if (!route) {
-    console.warn("Unknown SkillUp route:", routeName);
-    return;
-  }
+  if (!route) return console.warn("Unknown SkillUp route:", routeName);
   window.location.href = route;
 };
