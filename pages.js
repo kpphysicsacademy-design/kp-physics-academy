38c55de1d9505f4e72aaf2685f8c01a234badd83
kpphysicsacademy-design <kpphysicsacademy@gmail.com>
2026-08-29 07:57:15 +0530

    Create SkillUp central page registry for navigation and link auditing

---
+++ pages.js
@@ -0,0 +1,29 @@
+/* SkillUp Central Page Registry */
+window.SkillUpPages = {
+  main: [
+    "index.html",
+    "skillup-home.html",
+    "learn.html",
+    "practice.html",
+    "league.html",
+    "profile.html"
+  ],
+  compete: [
+    "league.html",
+    "challenge.html",
+    "challenge-concepts.html"
+  ],
+  system: [
+    "routes.js",
+    "navigation.js",
+    "common.css"
+  ]
+};
+
+/* Check whether a registered page exists in the central registry */
+window.isSkillUpPageRegistered = function(path) {
+  return Object.values(window.SkillUpPages)
+    .filter(Array.isArray)
+    .flat()
+    .includes(path);
+};
